7a54323aed9da79c0cc96c6db91cb22321bef522
Kadyn <kadynsr@gmail.com>
2022-10-05 07:55:35 -0600

    Initial commit

---
+++ src/main/java/net/kas/wanilla/WanillaClient.java
@@ -0,0 +1,10 @@
+package net.kas.wanilla;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WanillaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
